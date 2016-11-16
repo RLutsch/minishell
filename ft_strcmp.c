8162c32daa45b81cfab2c626791ac81fd2f11265
janhoon <jantjiehoon@gmail.com>
2016-11-16 16:35:35 +0200

    new cmp

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -12,22 +12,16 @@
 
 #include "21sh.h"
 
-int		ft_strcmp(char const *s1, char const *s2)
+int		ft_strcmp(const char *s1, const char *s2)
 {
-	size_t			i;
-	unsigned char	*tmp1;
-	unsigned char	*tmp2;
+	int		i;
 
-	tmp1 = (unsigned char *)s1;
-	tmp2 = (unsigned char *)s2;
 	i = 0;
-	while (tmp1[i] || tmp2[i])
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
 	{
-		if (tmp1[i] != tmp2[i])
-			return (1);
 		i++;
 	}
-	return (0);
+	return (s1[i] - s2[i]);
 }
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
